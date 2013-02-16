9fd4ca01375575f9656139edfb3a8dabc90fbd7a
Olivier Penhoat <openhoat@gmail.com>
2013-02-16 12:34:18 +0100

    clean

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,19 +3,44 @@ var fs = require('fs')
 
 module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-crypt');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.registerTask('default', ['uglify']);
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.registerTask('build', ['encrypt', 'uglify', 'copy']);
+  grunt.registerTask('default', ['clean', 'build']);
   var gruntConfig = {
-    pkg:'<json:package.json>',
+    pkg:grunt.file.readJSON('package.json'),
     clean:{
       default:'dist'
+    },
+    crypt:{
+      files:[
+        {
+          dir:'src',
+          include:'jqwerty.js',
+          encryptedExtension:'.encrypted'
+        }
+      ],
+      options:{
+        key:grunt.cli.options.key
+      }
+    },
+    copy:{
+      build:{
+        files:[
+          {
+            src:['dist/jqwerty.min.js'],
+            dest:'demo/jqwerty.min.js'
+          }
+        ]
+      }
     },
     uglify:{
       options:{
         compress:true,
-        mangle: true
+        mangle:true
       },
-      dist:{
+      build:{
         src:['src/jqwerty.js'],
         dest:'dist/jqwerty.min.js'
       }
